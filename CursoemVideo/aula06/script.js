89b2c785fa747fca1c9f1a29b1582700d69a7bd6
Allan kristhian figueira santos <100035050+allanfigueira@users.noreply.github.com>
2022-04-08 20:27:58 -0300

    projeto calculadora

--- CursoemVideo/aula06/script.js
+++ CursoemVideo/aula06/script.js
@@ -1,2 +1,23 @@
-var nome = prompt('Qual é seu nome?')// vai perguntar o nome da pessoa
+/*var nome = prompt('Qual é seu nome?')// vai perguntar o nome da pessoa
 alert(`é um grande prazer te conhecer ${nome}!`)
+var n1 = Number(prompt('Digite um numero: '))
+var n2 = Number(prompt('Digite outro numero: '))
+var s = n1+n2
+alert(`o valor da soma é ${s}`)
+*/
+
+
+
+function somar(){
+    var n1 = document.getElementById('txtn1')
+    var n2 = document.getElementById('txtn2')
+    var nome = document.getElementById('nome')
+    var nomes = nome.value
+    var num1 = Number(n1.value)
+    var num2 = Number(n2.value)
+    var s = num1+num2
+    
+    res.innerHTML = `<p>Olá ${nomes} seu nome posssui ${nomes.length} Letras</p>`
+    res.innerHTML += `<p>A soma entre os valores da: ${s}</p>`
+
+}
