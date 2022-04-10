33aefe0c96c52b521d38370b5c2fb17ec346111a
Allan kristhian figueira santos <100035050+allanfigueira@users.noreply.github.com>
2022-04-10 10:21:54 -0300

    .

--- Projetos/Calculadora/script.js
+++ Projetos/Calculadora/script.js
@@ -1,5 +1,22 @@
 function insert(num)
 {
-    document.getElementById('resultado').innerHTML += num;
+   var numero = document.getElementById('resultado').innerHTML;
+   document.getElementById('resuldado').innerHTML =  numero + num;
 
+}
+function clean()
+{
+    document.getElementById('resultado').innerHTML = '';
+}
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1)
+}
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado)
+    {
+        document.getElementById('resuldado').innerHTML = eval(resultado);
+    }
 }
