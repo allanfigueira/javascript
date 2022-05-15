36e92c2070559d05ab2a841f480d92daacfbf734
Allan kristhian figueira santos <100035050+allanfigueira@users.noreply.github.com>
2022-05-15 12:13:49 -0300

    .

--- CursoemVideo/ex4.js
+++ CursoemVideo/ex4.js
@@ -1,2 +1,6 @@
-var vel = 60.5
+var vel = 50
 console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log('você ultrapassou a velocidade permitida. Multado!')
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ ex12.js
@@ -0,0 +1,6 @@
+var hora = 8
+console.log (' agora são exatamente ${hora} horas.')
+
+if (hora < 12){
+    console.log
+}
