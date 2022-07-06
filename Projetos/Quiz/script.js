cdbd8f91f326fd525ebabe78272fd6d3d20fb250
Allan kristhian figueira santos <100035050+allanfigueira@users.noreply.github.com>
2022-07-06 12:37:23 -0300

    Update script.js

--- Projetos/Quiz/script.js
+++ Projetos/Quiz/script.js
@@ -1,20 +1,40 @@
-let pergunta = {
+let perguntas = 
+[
+{
+    titulo: 'Cachorro',
+    alternativas: ['Dog','drog','Rog','Bog'],
+    correta: 0
+},
+
+
+{
+    titulo: 'Fogo',
+    alternativas: ['Dirty','flares','flare','Fire'],
+    correta: 3
+}
+
+,
+{
     titulo: 'Gato',
     alternativas: ['Gat','Cat','Gate','Dog'],
     correta: 1
-};
+}
+]
+
 let app = {
 start: function(){
+        this.Atualpos = 0;
         let alts = document.querySelectorAll('.alternativa');
         alts.forEach((element,index)=>{
             element.addEventListener('click',()=>{
             this.checaResposta(index);
             })
         })
-app.mostraquestao(pergunta);
+app.mostraquestao(perguntas[this.Atualpos]);
 },
 
 mostraquestao: function(q){
+    this.qatual = q;
     //mostrando titulo
     let titleDiv = document.getElementById('titulo');
     titleDiv.textContent = q.titulo;
@@ -25,8 +45,22 @@ mostraquestao: function(q){
     })
 },
 
-checaResposta: function(user){
+Proximaperg: function(){
+    this.Atualpos++;
+    if(this.Atualpos == perguntas.length){
+        this.Atualpos = 0;
+    }
 
+},
+
+checaResposta: function(user){
+   if(this.qatual.correta == user){
+    console.log("correta")
+   }else{
+    console.log("Errada")
+   }
+    this.Proximaperg();
+    this.mostraquestao(perguntas[this.Atualpos]);
 }
 }
 app.start();
